a60e7dd6cd8b6acd7b6d799320c7f2f36ebf2dcd
Oscar <oscar_apaza07@hotmail.com>
2023-08-14 22:17:47 -0300

    test

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+app.use(express.static(path.resolve(__dirname,'public')))
+app.listen(PORT, () => console.log('running on' + PORT));
+app.get('/home', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})  
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})  
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})  
+app.post('/home', (req,res) => {
+    res.redirect('/home');
+})
